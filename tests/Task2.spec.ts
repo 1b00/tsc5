31ecafe4a6faa7b7100540530145a219517b6830
1b00 <1b00@pm.me>
2023-12-30 14:54:42 +0300

    task 2: rewritten

--- tests/Task2.spec.ts
+++ tests/Task2.spec.ts
@@ -1,8 +1,9 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, toNano } from 'ton-core';
+import { Cell, toNano, Address, TupleBuilder } from 'ton-core';
 import { Task2 } from '../wrappers/Task2';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
+import { pseudoRandomBytes } from 'crypto';
 
 describe('Task2', () => {
     let code: Cell;
@@ -35,4 +36,30 @@ describe('Task2', () => {
         // the check is done inside beforeEach
         // blockchain and task2 are ready to use
     });
+
+    it('should test', async () => {
+        const newOwner = new Address(0, pseudoRandomBytes(256 / 8));
+
+    });
+
+    it('fibonacci_sequence', async () => {
+        // the check is done inside beforeEach
+        // blockchain and task3 are ready to use
+        const tb = new TupleBuilder();
+        tb.writeNumber(201);
+        tb.writeNumber(4);
+
+        // let exp = tb.writeCell
+
+        const r = await blockchain.runGetMethod(task2.address, "test_case", tb.build())
+
+        // let rc = r.stackReader.readBigNumber()
+        console.log("gasUsed: ", r.gasUsed.toString())
+        // console.log("rc: ", rc.toString())
+        // let op = rc.beginParse().loadUint(32);
+        // console.log("loadBits: ", op.toString())
+
+        // expect(op).toBe(108)
+    });
+
 });
